d33f29741c4b95a9e65073a0384b84a163763ff0
bryanreinero <bryan.reinero@10gen.com>
2015-04-22 22:32:57 -0700

    Added javadocs for CircuitBreaker class

--- src/main/java/com/bryanreinero/firehose/circuitbreaker/CircuitBreaker.java
+++ src/main/java/com/bryanreinero/firehose/circuitbreaker/CircuitBreaker.java
@@ -16,6 +16,10 @@ public class CircuitBreaker {
 	private final AtomicBoolean isTripped = new AtomicBoolean(false);
 	private final Map<BreakerType, Threshold> tresholds = new HashMap<BreakerType, Threshold>();
 	
+	/**
+	 * Check the sample set. Trip the breaker if the threshold is exceeded
+	 * @param samples The sample set of operation metrics
+	 */
 	public void check( SampleSet samples ) {
 		for( Threshold t : tresholds.values() ) {
 			if( t.isExceeded( samples.report( key ) ) ) {
@@ -25,6 +29,10 @@ public class CircuitBreaker {
 		}
 	}
 	
+	/**
+	 * 
+	 * @return The set of thresholds this breaker is configured with
+	 */
 	public Set<Threshold> getThresholds() {
 		Set<Threshold> set = new HashSet<Threshold>();
 		for( Threshold t : tresholds.values()  ) 
@@ -32,33 +40,63 @@ public class CircuitBreaker {
 		return set;
 	}
 	
+	/**
+	 * Set a threshold which can trip the breaker
+	 * @param t
+	 */
 	public void setThreshold ( Threshold t ) {
 		tresholds.put( t.getType(), t );
 	}
 	
+	/**
+	 * Reset the breaker if it is tripped
+	 * @return
+	 */
 	public boolean reset () {
 		return isTripped.compareAndSet(false, true);
 	}
 	
+	/**
+	 * Check if the breaker is in a tripped state
+	 * @return
+	 */
 	public boolean isTripped() {
 		return isTripped.get();
 	}
 	
+	/**
+	 *  get the unique id for this individual breaker
+	 *  used for compare and swap modifications of the breaker
+	 * @return
+	 */
 	public UUID getUUID() {
 		return id;
 	}
 	
+	/**
+	 * 
+	 * @param key the name of the operation this breaker is guarding
+	 */
 	public CircuitBreaker( String key ) {
 		this.key = key;
 		id = UUID.randomUUID();
 	}
 	
+	/**
+	 * Used for copy on write modifications of the circuit breaker
+	 * @param id
+	 * @param key
+	 * @param tripped
+	 */
 	private CircuitBreaker( UUID id,  String key, Boolean tripped ) {
 		this.key = key;
 		this.id = id;
 		this.isTripped.set( tripped );
 	}
 	
+	/**
+	 * Deep copy of this circuit breaker for copy-on-write modification 
+	 */
 	@Override 
 	public CircuitBreaker clone() {
 		CircuitBreaker clone = new CircuitBreaker( this.id, this.key, isTripped.get() );
