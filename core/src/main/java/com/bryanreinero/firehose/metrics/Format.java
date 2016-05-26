8d77a6a447b6948ca252c1c9c18acf981a2bf54b
breinero <breinero@gmail.com>
2016-05-25 19:15:21 -0700

    fixed JSON string formatting

--- core/src/main/java/com/bryanreinero/firehose/metrics/Format.java
+++ core/src/main/java/com/bryanreinero/firehose/metrics/Format.java
@@ -2,6 +2,7 @@ package com.bryanreinero.firehose.metrics;
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -12,11 +13,17 @@ public class Format {
     public static String report( SampleSet set ) {
         StringBuffer buf = new StringBuffer("{\n ");
         buf.append("\"ops\": [\n");
-        for( Map.Entry<String, DescriptiveStatistics> aggregate : set.report().entrySet() ) {
-
-            DescriptiveStatistics stat = aggregate.getValue();
-            buf.append( "\t{ name: \""+aggregate.getKey()+"\",");
+        Iterator<Map.Entry<String, DescriptiveStatistics>> it = set.report().entrySet().iterator();
+        while( it.hasNext() ) {
+            buf.append( "{" );
+            Map.Entry<String, DescriptiveStatistics> entry = it.next();
+            DescriptiveStatistics stat = entry.getValue();
+            buf.append( "\"op\": \""+entry.getKey()+"\",\n");
             buf.append( formatStat( stat ) );
+            buf.append( "}" );
+
+            if( it.hasNext() )
+                buf.append(",");
 
         }
         buf.append( "\n\t]\n");
@@ -28,14 +35,13 @@ public class Format {
     public static String formatStat( DescriptiveStatistics stat ) {
 
         StringBuffer buf = new StringBuffer();
-        buf.append( "\n\t stats: {");
-        buf.append("\n\t\tmean: "+stat.getMean()+", \n");
-        buf.append("\t\tmedian: "+stat.getPercentile(50)+", \n");
-        buf.append("\t\tstd: "+stat.getStandardDeviation()+", \n");
-        buf.append("\t\tcount: "+stat.getN()+", \n");
-        buf.append("\t\ttotal: "+stat.getSum());
+        buf.append( "\n\t\"stats\": {");
+        buf.append("\n\t\t\"mean\": "+stat.getMean()+", \n");
+        buf.append("\t\t\"median\": "+stat.getPercentile(50)+", \n");
+        buf.append("\t\t\"std\": "+stat.getStandardDeviation()+", \n");
+        buf.append("\t\t\"count\": "+stat.getN()+", \n");
+        buf.append("\t\t\"total\": "+stat.getSum());
         buf.append("\n\t\t}");
-        buf.append("\n\t}");
         return buf.toString();
     }
 
